53a3cf15fca6f7dbec8f1c1f309c2a8596e2baa8
lukynski <lukas.mro@gmail.com>
2017-12-28 16:54:31 +0100

    commit 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! edited by visualstudio.code');
+  res.send('Hello World! edited by visualstudio.code again');
 });
 
 app.listen(port, function () {
